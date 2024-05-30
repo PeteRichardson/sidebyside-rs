80cdcdd7790874838246ff41d360a53e96f9a645
Pete Richardson <pete@peterichardson.com>
2024-05-30 10:34:24 -0700

    Add TUI.   Doesn't display anything, but q to quit works.
    Plan to refactor app code using https://github.com/ratatui-org/ratatui/blob/main/examples/colors_rgb.rs as a template.   Looks better

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,12 @@
 use clap::Parser;
 
+use crossterm::event::{self, Event, KeyCode, KeyEventKind};
+use ratatui::{
+    prelude::{Backend, Constraint, Layout, Terminal},
+    Frame,
+};
+use std::io;
+
 #[derive(Parser, Debug)]
 #[command(version, about, long_about=None)]
 pub struct Config {
@@ -7,4 +14,37 @@ pub struct Config {
     pub file1: String,
     #[arg(default_value_t = String::from("File2"))]
     pub file2: String,
+}
+
+pub struct App {}
+
+impl App {
+    pub fn new() -> App {
+        App {}
+    }
+}
+
+pub fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
+    loop {
+        terminal.draw(|f| ui(f, &mut app))?;
+
+        if let Event::Key(key) = event::read()? {
+            if key.kind == KeyEventKind::Press {
+                match key.code {
+                    KeyCode::Char('q') | KeyCode::Esc => return Ok(()),
+                    _ => {}
+                }
+            }
+        }
+    }
+}
+
+fn ui(f: &mut Frame, _app: &mut App) {
+    let _rects = Layout::horizontal([Constraint::Min(5), Constraint::Min(5)]).split(f.size());
+
+    // render_file(f, app, rects[0]);
+    // render_scrollbar(f, app, rects[0]);
+
+    // render_file(f, app, rects[1]);
+    // render_scrollbar(f, app, rects[1]);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use clap::Parser;
+use crossterm::{
+    event::{DisableMouseCapture, EnableMouseCapture},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
 use env_logger;
 use log::debug;
-use sidebyside::Config;
+use ratatui::prelude::{CrosstermBackend, Terminal};
+use sidebyside::{run_app, App, Config};
+use std::{error::Error, io};
 
 fn setup_logging() {
     env_logger::builder()
@@ -11,9 +18,34 @@ fn setup_logging() {
         .init();
 }
 
-fn main() {
-    let config = Config::parse();
-
+fn main() -> Result<(), Box<dyn Error>> {
     setup_logging();
-    debug!("Comparing {} and {}...", config.file1, config.file2);
+    let config = Config::parse();
+    debug!("{:?}", config);
+
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    // create app and run it
+    let app = App::new();
+    let res = run_app(&mut terminal, app);
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    if let Err(err) = res {
+        println!("{err:?}");
+    }
+
+    Ok(())
 }
